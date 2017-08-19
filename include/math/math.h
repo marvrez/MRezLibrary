dfb829863e401621339938ea3101d9d16295c918
Marvin Reza <marvinreza@outlook.com>
2017-08-19 15:07:21 +0200

    Added fast inverse square root, and also added internal function linkage to all functions.

--- include/math/math.h
+++ include/math/math.h
@@ -10,18 +10,18 @@
 namespace Math {
 //Defines
 #define M_PI            3.1415926535897932384626433832795f
-#define RAD(val)        ((val)*0.0174532925199432957692369076848f)
-#define DEG(val)        ((val)*57.295779513082320876798154814105f)
-#define LARGE_EPSILON   1e-2f
-#define EPSILON         1e-4f
-#define TINY_EPSILON    1e-5f
+#define M_RAD(val)        ((val)*0.0174532925199432957692369076848f)
+#define M_DEG(val)        ((val)*57.295779513082320876798154814105f)
+#define M_LARGE_EPSILON   1e-2f
+#define M_EPSILON         1e-4f
+#define M_TINY_EPSILON    1e-5f
 #define M_INFINITE      3.4e38f
 #define M_EULER_NUMBER  2.71828182845904523536f
 #define M_SQRT2         1.4142135623730950488016887242097f
 #define M_SQRT3         1.7320508075688772935274463415059f
 
 //(kinda optimized) math functions
-float fSinCosRecurse (float ret, float rad, const float rad2, float radpow, float fact, const int prec, int cur) {
+inline float fSinCosRecurse (float ret, float rad, const float rad2, float radpow, float fact, const int prec, int cur) {
     radpow *= -rad2;
     fact   *= cur++;
     fact   *= cur++;
@@ -34,7 +34,7 @@ float fSinCosRecurse (float ret, float rad, const float rad2, float radpow, floa
         return ret;
 }
 
-float fSin (float rad, int precision = 4) {
+inline float fSin (float rad, int precision = 4) {
     while(rad >  M_PI) rad -= (M_PI*2.0f);
     while(rad < -M_PI) rad += (M_PI*2.0f);
 
@@ -44,7 +44,7 @@ float fSin (float rad, int precision = 4) {
     return fSinCosRecurse(ret, rad, rad2, rad, 1.0f, (precision << 1) + 1, 2);
 }
 
-float fCos (float rad, int precision = 4) {
+inline float fCos (float rad, int precision = 4) {
     while(rad >  M_PI) rad -= (M_PI*2.0f);
     while(rad < -M_PI) rad += (M_PI*2.0f);
 
@@ -54,22 +54,22 @@ float fCos (float rad, int precision = 4) {
     return fSinCosRecurse(ret, rad, rad2, 1.0f, 1.0f, (precision << 1) + 1, 1);
 }
 
-inline bool fEqual(float f1, float f2, float precision = LARGE_EPSILON) {
+inline bool fEqual(float f1, float f2, float precision = M_LARGE_EPSILON) {
     return fabsf(f1-f2) <= precision;
 }
 
-int factorial(int n) {
+inline int factorial(int n) {
     int res = 1;
     for (int i = 1; i <= n; ++i)
         res *= i;
     return res;
 }
 
-int digitSum(int n) {
+inline int digitSum(int n) {
     return n ? (n%10) + digitSum(n/10) : 0;
 }
 
-unsigned long long combination(unsigned long long n, unsigned long long k) {
+inline unsigned long long combination(unsigned long long n, unsigned long long k) {
     if (k > n) return 0;
 
     if (k > n/2) k = n-k;
@@ -82,7 +82,7 @@ unsigned long long combination(unsigned long long n, unsigned long long k) {
     return r;
 }
 
-std::pair<std::complex<float>, std::complex<float> > solveQuadratic(float a, float b, float c) {
+inline std::pair<std::complex<float>, std::complex<float> > solveQuadratic(float a, float b, float c) {
     std::complex<float> x1 = 0, x2 = 0;
     float determinant = b*b - (4 * a*c);
 
@@ -105,12 +105,12 @@ std::pair<std::complex<float>, std::complex<float> > solveQuadratic(float a, flo
     return std::make_pair(x1, x2);
 }
 
-static bool doIntersect(Vector2f p1, Vector2f q1, Vector2f p2, Vector2f q2) {
+inline bool doIntersect(Vector2f p1, Vector2f q1, Vector2f p2, Vector2f q2) {
     static auto det = [](const Vector2f& u, const Vector2f& v) {return u.X()*v.Y() - u.Y()*v.X();};
     return (det(p2-p1, q1-p1)*det(p2-p1, q2-p1) < 0) && (det(q2-q1, p1-q1)*det(q2-q1, p2-q1) < 0);
 }
 
-static Vector2f getIntersection(Vector2f p1, Vector2f q1, Vector2f p2, Vector2f q2) {
+inline Vector2f getIntersection(Vector2f p1, Vector2f q1, Vector2f p2, Vector2f q2) {
     if(doIntersect(p1,q1,p2,q2)) {
         if(p2.X() - p1.X() != 0 && q2.X() - q1.X() != 0) {
             float a1 = (p2.Y() - p1.Y())/(p2.X() - p1.X());
@@ -151,6 +151,17 @@ inline float gaussianFunction(float maxVal, float wideness, float x) {
 inline float clamp(float val, float lower, float upper) {
     return std::max(lower, std::min(val, upper));
 }
+
+inline float fastInvSqrt(float x) {
+    //https://en.wikipedia.org/wiki/Fast_inverse_square_root
+    float xhalf = 0.5f * x;
+    int i = *(int*)&x;          // Integer representation of float
+    i = 0x5f3759df - (i >> 1);  // Initial guess
+    x = *(float*)&i;            // Converted back to floating point
+    x = x*(1.5f-(xhalf*x*x));   // One round of Newton-Raphson's method
+    return x;
+}
+
 
 
 } //namespace Math;
