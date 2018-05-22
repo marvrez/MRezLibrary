be08b4842b274580f548c255fa2404ad430f8912
Marvin Reza <marvinreza@outlook.com>
2018-05-22 13:25:35 +0200

    Added example of estimating pi using monte carlo

--- main.cpp
+++ main.cpp
@@ -1,13 +1,42 @@
-#include "humanize/time.h"
-
+#include <algorithm>
+#include <cstdio>
 #include <iostream>
-#include <chrono>
-#include <assert.h>
-using namespace std;
+#include <vector>
+#include <math/matrix.h>
+#include <math/math.h>
+#include "humanize/time.h"
+#include "misc./log.h"
+#include "misc./color.h"
+#include "misc./string_utils.h"
+#include "misc./rng.h"
+#include "misc./array_util.h"
+#include "misc./monte_carlo.h"
+#include "network/net.h"
+
+#ifdef THREADS
+#include <thread>
+#include <mutex>
+#include <atomic>
+#endif
+
 
 int main() {
-    auto res = humanize::time::relativeTime(std::chrono::system_clock::now() + std::chrono::seconds(312312323), std::chrono::system_clock::now() + std::chrono::minutes(50),
-                                            humanize::SuffixType::LONG, 3);
-    std::cout << res.str << "\n";
-    return 0;
+    // Sampling function. Provides a callable random number generator
+    // and expects a data type (double for this case) return value.
+    auto sampling_func = [] (const std::function<double()>& rng) -> double {
+        // Sample a point within the unit square i.e. x and y in [0,1].
+        auto x = rng(), y = rng();
+
+        // Calculate distance from the origin.
+        auto distance = sqrtf(x * x + y * y);
+
+        // If the point is within the quarter circle, append 1.0.
+        return distance <= 1.0 ? 1.0 : 0.0;
+    };
+
+    MonteCarlo<double> pi_estimator(sampling_func);
+
+    // Apply 5 *10 ^ times.
+    MEASURE_TIME(printf("%.6f\n", 4*pi_estimator.simulate(5e8)));
+  return 0;
 }
